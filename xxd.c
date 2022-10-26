827a156c46f29df032db37f03aacb3e6e9a6eb0a
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2022-10-26 14:51:33 +0200

    bullshit encore pour certaines situations..

--- xxd.c
+++ xxd.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 void writeOnFich(FILE *f_entree)
 {
     int c;
     int c2;
     int octet=0;
-    char *tab = malloc(16*sizeof(char));
     int nbSpace=0;
+    char *tab = malloc(16*sizeof(char));
+    bool endLine=true;
     if (f_entree == stdin) {
         // TODO: read from stdin and write to stdout when octet%16 == 0
-        printf("TODO: stdin xxd");
+        printf("TODO: stdin xxd\n");
     }else {
         while ((c = fgetc(f_entree)) != EOF) {
+            if (endLine == true) {
+                fprintf(stdout, "%08x:", octet);
+            }
+            endLine=false;
             if ((c2 = fgetc(f_entree)) != EOF) {
-                if (octet == 0) {
-                    fprintf(stdout, "%08x:", 0);
-                }
+
                 fprintf(stdout, " %02x%02x", c, c2);
                 if (isprint(c) != 0) {
                     tab[octet%16] = c;
@@ -31,45 +35,47 @@ void writeOnFich(FILE *f_entree)
                 }
                 octet=octet+2;
                 if (octet%16 == 0) {
-                    fprintf(stdout, "  %s", tab);
-                    fprintf(stdout, "\n%08x:", octet);
+                    fprintf(stdout, "  %s\n", tab);
+                    endLine = true;
                 }
-                if (tab[15] != '\0') {
+                if (tab[15] != 0) {
                     for (int i=0; i<=15; i++){
-                        tab[i] = '\0';
+                        tab[i] = 0;
                     } 
                 }
             }else {
-                fprintf(stdout, " %02x ", c);
+                fprintf(stdout, " %02x", c);
                 octet++;
-                nbSpace = 40-(((octet%16)*2)+((octet%16)/2));
-                fprintf(stdout, "%d", nbSpace);
+                nbSpace = 40-((octet%16)*2 + ((octet/16)));
                 for (int i=0; i<nbSpace; i++) {
                     fprintf(stdout, " ");
                 }
                 if (isprint(c) != 0) {
                     tab[octet%16] = c;
-                }else {
+                }else { 
                     tab[octet%16] = '.';
-                }
-                fprintf(stdout, " \n%s", tab);
-                fprintf(stdout, "  %s", tab);
+                };
+
+                fprintf(stdout, "  %s\n", tab);
                 for (int i=0; i<=15; i++){
-                    tab[i] = '\0';
+                    tab[i] = 0;
                 }
             }
         }
-        if((c=fgetc(f_entree)) == EOF && (tab[0] != '\0')) {
-            fprintf(stdout, "test2");
-                    
+        if((c=fgetc(f_entree)) == EOF && (tab[1] != 0) && (tab[2] != 0)) {
             nbSpace = 40-(((octet%16)*2)+((octet%16)/2));
+            
             for (int i=0; i<nbSpace; i++) {
                 fprintf(stdout, " ");
             }
-            fprintf(stdout, "  %s", tab);
+            if (isprint(c) != 0) {
+                tab[octet%16] = c;
+            }else {
+                tab[octet%16] = '.';
+            }
+            fprintf(stdout, "  %s\n", tab);
         }
     }
-    fprintf(stdout, "\n");
 }
 
 int main(int argc, char *argv[])
